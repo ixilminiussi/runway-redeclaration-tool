d4741f31ffdb50137d4bd5237e2e5b2ff40150f1
Stefan Wiecek <dev@wiecek.co.uk>
2021-03-08 17:30:56 +0000

    Added JavaDocs

--- src/importXML.java
+++ src/importXML.java
@@ -12,6 +12,10 @@ public class importXML {
     private Document doc;
 
 
+    /**
+     * @param filename The filename of the XML file to be imported
+     * @throws Exception
+     */
     importXML(String filename) throws Exception {
         this.filename = filename;
 
@@ -30,6 +34,11 @@ public class importXML {
         }
     }
 
+
+    /**
+     * Function that imports all runways from an XML file
+     * @return An ArrayList of Runway objects
+     */
     public ArrayList<Runway> importRunwaysFromXML() {
         // get a list of all runways from the XML files
         NodeList runwayNodes = doc.getElementsByTagName("runway");
@@ -49,6 +58,10 @@ public class importXML {
     }
 
 
+    /**
+     * Function that imports all runways from an XML file
+     * @return An ArrayList of Runway objects
+     */
     public ArrayList<Obstruction> importObstructionsFromXML() {
         // get a list of all runways from the XML files
         NodeList obstructionNodes = doc.getElementsByTagName("obstruction");
@@ -67,6 +80,12 @@ public class importXML {
         return obstructionObjects;
     }
 
+
+    /**
+     * Function that, given an obstruction node, returns an obstruction object
+     * @param n The node from the XML file
+     * @return An Obstruction object
+     */
     // function that, given an obstruction element, returns an obstruction object
     private Obstruction newObsFromElement (Element n) {
         String name = getTagValue(n,"name");
@@ -77,7 +96,12 @@ public class importXML {
         return new Obstruction(name, height, length, distanceFromThreshold, distanceFromCentre);
     }
 
-    // function that, given a runway element, returns a runway element
+
+    /**
+     * Function that, given a runway node, returns a runway object
+     * @param n The node from the XML file
+     * @return A Runway object
+     */
     private Runway newRunwayFromElement (Element n) {
         String name = getTagValue(n,"name");
         String airport = getTagValue(n, "airport");
@@ -85,7 +109,7 @@ public class importXML {
         int TODA = getPositiveIntTagValue(n,"TODA");
         int ASDA = getPositiveIntTagValue(n,"ASDA");
         int LDA = getPositiveIntTagValue(n,"LDA");
-        int displacedThreshold = getPositiveIntTagValue(n,"displacedThreshold");
+        int displacedThreshold = getIntTagValue(n,"displacedThreshold");
         int stripEnd = getPositiveIntTagValue(n,"stripEnd");
         int EGR = getPositiveIntTagValue(n,"EGR");
         int RESA = getPositiveIntTagValue(n,"RESA");
@@ -98,11 +122,27 @@ public class importXML {
                 blastAllowance, stopway, clearway, direction);
     }
 
+
+    /**
+     * Parses and returns the String value held in the XML file for a given tag
+     * If a node has two of the same tag, the first will be returned.
+     * @param n The element parsed from the XML file
+     * @param tag The XML tag that is being searched for
+     * @return The String value in the tag for the element
+     */
     private String getTagValue(Element n, String tag) {
         return n.getElementsByTagName(tag).item(0).getTextContent();
     }
 
-    private int getPositiveIntTagValue(Element n, String tag) {
+
+    /**
+     * Parses the value stored in the XML file and returns a positive integer.
+     * @param n The element parsed from the XML file
+     * @param tag The XML tag that is being searched for
+     * @return The positive integer (or zero) stored in the XML file
+     * @throws IllegalArgumentException If the parsed integer value is negative
+     */
+    private int getPositiveIntTagValue(Element n, String tag) throws IllegalArgumentException {
         int value =  Integer.parseInt(getTagValue(n, tag));
         if (value < 0) {
             throw new IllegalArgumentException(tag + " value cannot be negative");
@@ -110,15 +150,28 @@ public class importXML {
         return value;
     }
 
+
+    /**
+     * Parses the value stored in the XML file and returns a  integer.
+     * @param n The element parsed from the XML file
+     * @param tag The XML tag that is being searched for
+     * @return The integer value stored in the XML file
+     * @throws IllegalArgumentException If the parsed integer value is negative
+     */
     private int getIntTagValue(Element n, String tag) {
         return Integer.parseInt(getTagValue(n, tag));
     }
 
     public static void main(String args[]) throws Exception {
-        importXML testing = new importXML("src/test3runways.xml");
-        ArrayList<Runway> runwaysTest = testing.importRunwaysFromXML();
-        for (Runway runway : runwaysTest) {
-            System.out.println(runway.getName() + " - TORA: " + runway.getTORA());
+        importXML testing = new importXML("src/test3obs.xml");
+        ArrayList<Obstruction> obsTest = testing.importObstructionsFromXML();
+        for (Obstruction obs : obsTest) {
+            System.out.println(obs.getName() + " - Height: " + obs.getHeight());
+        }
+        importXML runTest = new importXML("src/test3runways.xml");
+        ArrayList<Runway> runwaysTest = runTest.importRunwaysFromXML();
+        for (Runway r : runwaysTest) {
+            System.out.println(r.getName() + " - TORA: " + r.getTORA());
         }
     }
 }
