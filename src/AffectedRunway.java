4bf569ba9485b1d5fb7b78d8349db1c39d6bbdde
cr2g18 <cr2g18@soton.ac.uk>
2021-04-22 00:04:04 +0000

    Tiny calculation change

--- src/AffectedRunway.java
+++ src/AffectedRunway.java
@@ -1,3 +1,5 @@
+package sample;
+
 public class AffectedRunway {
     private int TORA, TODA, ASDA, LDA;
     private Runway originalRunway;
@@ -5,20 +7,25 @@ public class AffectedRunway {
 
     AffectedRunway(Runway originalRunway, Obstruction obstruction) {
         this.originalRunway = originalRunway;
+        System.out.println("Affected Runway: " + this.originalRunway);
         this.obstruction = obstruction;
         recalculate(obstruction);
     }
 
     public void recalculate (Obstruction obstruction) {
+
         // replace obstruction and change all values but original runway is the same
         // landing over / take off away
         this.obstruction = obstruction;
+
+
         if (obstruction.getDistanceFromThreshold() < (originalRunway.getTORA() + originalRunway.getDisplacedThreshold())/2) {
             TORA = Math.min(originalRunway.getTORA() - originalRunway.getBlastAllowance() - obstruction.getDistanceFromThreshold() - originalRunway.getDisplacedThreshold(),
                     originalRunway.getTORA() - originalRunway.getStripEnd() - originalRunway.getRESA() - obstruction.getDistanceFromThreshold());
             ASDA = TORA + originalRunway.getStopway();
             TODA = TORA + originalRunway.getClearway();
-            LDA = originalRunway.getLDA() - obstruction.getDistanceFromThreshold() - originalRunway.getStripEnd() - (obstruction.getHeight() * originalRunway.getSlopeRatio());
+            LDA = originalRunway.getLDA() - obstruction.getDistanceFromThreshold() - originalRunway.getStripEnd() - (obstruction.getHeight() * 50);
+
         }
         //landing towards, take off towards
         else{
@@ -26,6 +33,7 @@ public class AffectedRunway {
             ASDA = TORA;
             TODA = TORA;
             LDA = obstruction.getDistanceFromThreshold() - originalRunway.getRESA() - originalRunway.getStripEnd();
+            System.out.println("NEW TORA: " + originalRunway.getTORA());
         }
     }
 
